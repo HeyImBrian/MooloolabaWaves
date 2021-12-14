eafa9a822f9a7b1e81509e4671cf39b81a496968
HeyImBrian <78251774+HeyImBrian@users.noreply.github.com>
2021-12-14 15:47:16 -0800

    Predicting same values repeatedly

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import pandas as pd
 import csv
 from io import StringIO
 
+import datetime
+
+from sklearn.datasets import make_regression
 
 
 # Load csv into different variables
@@ -22,38 +25,47 @@ waveRowsStrings = pd.read_csv(dataFile, skiprows=0, usecols=[0])
 waveAttributesFloats = ["significant height","max height","zero upcrossing wave period","peak energy wave period","peak direction","temperature"]
 waveRowsFloats = np.loadtxt(dataFile, delimiter=",", skiprows=1, usecols=(1, 2, 3, 4, 5, 6))
 
-allAttributes = ["day", "significant height","max height","zero upcrossing wave period","peak energy wave period","peak direction","temperature"]
-
-
-# Combining each set of times into single day elements.
-
-def advanceDay(day):
-    day += 1
-    if day > 365:
-        day = 1
-    return day
+allAttributes = ["month", "day", "year", "hour", "min", "significant height","max height","zero upcrossing wave period","peak energy wave period","peak direction","temperature"]
 
 
 
+
+
+
+# Setting up the time columns
+
+currDateTime = datetime.datetime(month=1, day=1, year=2017, hour=1, minute=0)
 daysList = []
-currDay = 1
+monthList = []
+yearList = []
+hourList = []
+minList = []
+
 currDataIndex = 0
 maxDataIndex = waveRowsStrings.size
 
 while currDataIndex < maxDataIndex:
+
     for i in range(0, 46):
         if currDataIndex < maxDataIndex:
-            daysList.append([currDay])
+            daysList.append([currDateTime.day])
+            monthList.append([currDateTime.month])
+            yearList.append([currDateTime.year])
+            hourList.append([currDateTime.hour])
+            minList.append([currDateTime.minute])
+
+            currDateTime += datetime.timedelta(minutes=30)
             currDataIndex += 1
         else:
             break
 
-    currDay = advanceDay(currDay) # After looping through all the times in a day, go to next day.
 
 
 
 
-# Need to remove rows with -99. Will round out the values by using surrounding ones because numpy cannot pop values
+
+
+# Need to remove rows with -99.
 for index, row in enumerate(waveRowsFloats):
     for indexInRow, value in enumerate(row):
         if value == -99.9:
@@ -66,25 +78,27 @@ for index, row in enumerate(waveRowsFloats):
 
 
 
-
-# Appending the days to the beginning of the waveRowsFloats.
+# Appending the all attributes to the beginning of the waveRowsFloats.
 # This effectively replaces the date/time in the original data.
-dataTemp = np.append(daysList, waveRowsFloats, axis=1)
+dataTemp = np.append(minList, waveRowsFloats, axis=1)
+dataTemp = np.append(hourList, dataTemp, axis=1)
+dataTemp = np.append(yearList, dataTemp, axis=1)
+dataTemp = np.append(daysList, dataTemp, axis=1)
+dataTemp = np.append(monthList, dataTemp, axis=1)
+
+
 data = pd.DataFrame(dataTemp, columns=allAttributes)
 
 
-
-
-
-plt.scatter(dataTemp[:, 0], dataTemp[:, 6])
-plt.show()
+# plt.scatter(dataTemp[:, 0], dataTemp[:, 6])
+# plt.show()
 
 
 # y is the temperature
 train, test = train_test_split(data, random_state=42)
-xTrain = train[train.columns[0:3]]
+xTrain = train[train.columns[0:5]]
 yTrain = train['temperature']
-xTest = test[test.columns[0:3]]
+xTest = test[test.columns[0:5]]
 yTest = test['temperature']
 
 
@@ -94,15 +108,35 @@ scaler.fit(xTrain)
 xTrain = scaler.transform(xTrain)
 xTest = scaler.transform(xTest)
 
+print("Loading")
 
-
-neuralNetwork = MLPRegressor(hidden_layer_sizes=(10, 10, 10), max_iter=1000, activation='tanh')
+neuralNetwork = MLPRegressor(hidden_layer_sizes=(10, 10, 10), max_iter=1000, activation='logistic')
 neuralNetwork.fit(xTrain, yTrain)
 
-testData = [[250,0.774,1.17]]
+#  "month", "day", "year", "hour", "min",
+# testData = [[8, 4, 2018, 1, 0]]
+# predictions = neuralNetwork.predict(testData)
+# print(predictions)
 
-predictions = neuralNetwork.predict(testData)
-print(predictions)
+
+
+# # still testing
+# Xnew, _ = make_regression(n_samples=3, n_features=5, noise=1, random_state=1)
+# ynew = neuralNetwork.predict(Xnew)
+# for i in range(len(Xnew)):
+# 	print("X=%s, Predicted=%s" % (Xnew[i], ynew[i]))
+
+# big test
+testList = []
+for index, i in enumerate(range(0, 12)):
+    tempTest = np.array([index+1, 1.0, 2017.0, 1.0, 0.0])
+    testList.append(tempTest)
+
+bruh = neuralNetwork.predict(testList)
+
+for i in range(len(testList)):
+    print(testList[i], bruh[i])
+
 
 print("Accuracy Test: ", neuralNetwork.score(xTest, yTest))
 
